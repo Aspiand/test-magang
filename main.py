dc0566855ca61fb5d54d46bceb9762c045b5da58
Aspian <muhammad.aspian.d@gmail.com>
2025-04-12 09:19:47 +0800

    ....

--- main.py
+++ main.py
@@ -6,15 +6,20 @@ sites = [
     "https://github.com",
     "https://www.google.com",
     "https://www.facebook.com",
-    "https://www.youtube.com"
+    "https://www.youtube.com",
 ]
 
-while True:
-    sleep(5)
 
+def main():
     status = list(map(requests.get, sites))
     with open("log.txt", "w") as f:
         now = datetime.now()
         for site, s in zip(sites, status):
             f.write(f"{now}: {site} - {s.status_code}\n")
     print("Log updated.")
+
+
+if __name__ == "__main__":
+    while True:
+        sleep(5)
+        main()
